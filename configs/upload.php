1e910af54f418d0758be29374e569b84b5e10361
skyprogramer <x87256506@126.com>
2015-06-30 22:45:52 +0800

    图片处理类

--- configs/upload.php
+++ configs/upload.php
@@ -2,6 +2,9 @@
 require substr(dirname(__FILE__),0,-7).'/init.inc.php';
 if (isset($_POST['send'])) {
   $_fileupload = new FileUpload_inc('pic',$_POST['MAX_FILE_SIZE']);
-  echo $_fileupload->getPath();
+  $_path = $_fileupload->getPath();
+  $_img = new Image_inc($_path);
+  $_img->thumb(150,100);
+  $_img->out();
 }
 ?>
